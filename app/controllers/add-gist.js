46ead41349c554c8ab1bd380de6a2988ab24d3dd
pranavliag <pranavmechary@gmail.com>
2020-08-17 22:17:29 +0530

    feat(component): add header and gist details

---
+++ app/controllers/add-gist.js
@@ -0,0 +1,52 @@
+import Controller from '@ember/controller';
+
+export default class AddGistController extends Controller {
+
+  gists = [ {
+      fileName: "example.hbs",
+      description: "just an example",
+      body: "<p>An <strong>example</strong> for {{username}}</p>"
+    },
+
+    {
+      fileName: "example.hbs",
+      description: "2nd example",
+      body: `
+                import Controller from "@ember/controller";
+                import {
+                  action
+                } from "@ember/object";
+                import {
+                  inject as service
+                } from "@ember/service";
+
+                export default class AddGistController extends Controller {
+                  @service gistsInfo;
+                }
+`
+    },
+    {
+      fileName: "example.hbs",
+      description: "3rd example",
+      body: `
+                import Controller from "@ember/controller";
+                import {
+                  action
+                } from "@ember/object";
+                import {
+                  inject as service
+                } from "@ember/service";
+
+                export default class AddGistController extends Controller {
+              @service gistsInfo;
+                }
+`
+    }, {
+      fileName: "fourth-example.hbs",
+      description: "some hbs",
+      body: `good day for sme {{variable}}`
+    }
+  ]
+
+
+}
